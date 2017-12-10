aa7d8c1eb9daabeb39c902d4490af23fcfce36f8
zypeh <zypeh.geek@gmail.com>
2017-12-10 17:11:05 +0800

    ✏️ Pull function initial; unknown error

--- src/Packrunner.hs
+++ src/Packrunner.hs
@@ -6,12 +6,15 @@ import System.Environment
 import Paths_packrunner (version)
 import Data.Version (showVersion)
 
+import Packrunner.Pull (pull)
+
 packrunner :: IO ()
 packrunner = do
     args <- getArgs
     case args of
         ["help"] -> usage
         ["version"] -> putStrLn $ unwords prVersion
+        ["pull"] -> pull
         _ -> usage
     where
         prVersion = ["Packrunner (prpr)", showVersion version]
@@ -23,5 +26,6 @@ usage = do
         concat ["Usage: ", prog, " [OPTION]"] : [
         "COMMANDS ",
         "    help                Print this message",
-        "    version             Print the version number"
+        "    version             Print the version number",
+        "    pull                Pull from the registry"
         ]
---
+++ src/Packrunner/Pull.hs
@@ -0,0 +1,10 @@
+module Packrunner.Pull (pull) where
+
+import Data.ByteString.Char8
+import Data.Either.Unwrap
+import Network.Download (openURI)
+
+pull :: IO ()
+pull = do 
+    src <- openURI "http://google.com"
+    Data.ByteString.Char8.putStrLn (fromRight src)
